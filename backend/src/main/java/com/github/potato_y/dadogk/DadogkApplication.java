91a10e3d3df3783714006353e0d946f4e5fa0233
Potato-Y <projectpotatoy@gmail.com>
2023-10-12 14:52:29 +0900

    백앤드 프로젝트 추가

---
+++ backend/src/main/java/com/github/potato_y/dadogk/DadogkApplication.java
@@ -0,0 +1,13 @@
+package com.github.potato_y.dadogk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadogkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadogkApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/potato_y/dadogk/DadogkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.potato_y.dadogk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadogkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
