42082ccc1869a469642d508d753845c9d9ad5e73
Potato-Y <projectpotatoy@gmail.com>
2023-11-26 14:45:27 +0900

    refact: 주입 관련 개선

--- backend/src/main/java/com/github/dadogk/socket/WebSocketConfiguration.java
+++ backend/src/main/java/com/github/dadogk/socket/WebSocketConfiguration.java
@@ -5,23 +5,20 @@ import com.github.dadogk.study.StudyConnectionHandler;
 import com.github.dadogk.study.StudyService;
 import com.github.dadogk.user.util.UserUtil;
 
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSocket
 public class WebSocketConfiguration implements WebSocketConfigurer {
     private final StudyService studyService;
     private final TokenProvider tokenProvider;
     private final UserUtil userUtil;
-
-    public WebSocketConfiguration(StudyService studyService, TokenProvider tokenProvider, UserUtil userUtil) {
-        this.studyService = studyService;
-        this.tokenProvider = tokenProvider;
-        this.userUtil = userUtil;
-    }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
