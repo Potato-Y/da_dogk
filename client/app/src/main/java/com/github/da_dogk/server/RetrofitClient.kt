fdd78fed31bb55a01ad4a31e9536031d29752084
Dongsu Lee <dongsu0717@nate.com>
2023-12-19 17:24:28 +0900

    .

--- client/app/src/main/java/com/github/da_dogk/server/RetrofitClient.kt
+++ client/app/src/main/java/com/github/da_dogk/server/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.lang.reflect.Type
-
+//a
 object RetrofitClient {
     private const val BASE_URL = "https://dadogk2.duckdns.org/api/"
 
