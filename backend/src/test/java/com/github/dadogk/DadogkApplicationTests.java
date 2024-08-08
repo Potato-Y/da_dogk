9e06524d449133a2f3a423429a66bc9e49794a65
Potato-Y <projectpotatoy@gmail.com>
2024-08-08 23:52:44 +0900

    rename: 파일 위치 이동 프로젝트 패키지명에 맞추어 파일 위치를 변경

--- backend/src/test/java/com/github/dadogk/DadogkApplicationTests.java
+++ backend/src/test/java/com/github/dadogk/DadogkApplicationTests.java
@@ -1,4 +1,4 @@
-package com.github.potato_y.dadogk;
+package com.github.dadogk;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DadogkApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
